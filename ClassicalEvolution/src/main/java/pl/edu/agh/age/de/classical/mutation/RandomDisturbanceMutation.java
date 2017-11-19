27c6d53c0379e6a02a3098ea3ddb09485f3a1489
bgrochal <bartek.grochal@gmail.com>
2017-11-19 20:48:25 +0100

    Mutation and Recombination operators for the Classical Algorithm added.

---
+++ ClassicalEvolution/src/main/java/pl/edu/agh/age/de/classical/mutation/RandomDisturbanceMutation.java
@@ -0,0 +1,73 @@
+package pl.edu.agh.age.de.classical.mutation;
+
+import pl.edu.agh.age.compute.stream.emas.reproduction.mutation.Mutation;
+import pl.edu.agh.age.compute.stream.emas.solution.DoubleVectorSolution;
+import pl.edu.agh.age.de.common.solution.DoubleVectorSolutionFactory;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * This class implements the mutation operator for Classical EMAS Algorithm. This mutation strategy draws approximately
+ * {@link #mutationRate} percent of genes and performs random disturbance of selected genes' values.
+ *
+ * @author Bartłomiej Grochal
+ */
+public class RandomDisturbanceMutation implements Mutation<DoubleVectorSolution> {
+
+	private final DoubleVectorSolutionFactory solutionFactory;
+	private final Random randomGenerator;
+
+	private final double mutationRate;
+	private final double disturbanceRange;
+
+
+	/**
+	 * @param mutationRate     A portion of genes to be mutated.
+	 * @param disturbanceRange A range of disturbance of a gene's value (i.e. for a gene with given <b>value</b>, its
+	 *                         disturbance is a random number not lesser than <b>value - disturbanceRange</b> and not
+	 *                         greater than <b>value + disturbanceRange</b>).
+	 */
+	public RandomDisturbanceMutation(final DoubleVectorSolutionFactory solutionFactory, final double mutationRate,
+									 final double disturbanceRange) {
+		checkArgument(0 < mutationRate && 1 >= mutationRate);
+		checkArgument(0 < disturbanceRange);
+
+		this.solutionFactory = solutionFactory;
+		randomGenerator = ThreadLocalRandom.current();
+
+		this.mutationRate = mutationRate;
+		this.disturbanceRange = disturbanceRange;
+	}
+
+
+	/**
+	 * Performs a mutation of a genotype belonging to given {@code solution}.
+	 */
+	@Override
+	public DoubleVectorSolution mutate(final DoubleVectorSolution solution) {
+		final double[] mutationProbabilities = randomGenerator.doubles(solution.length()).toArray();
+
+		final double[] mutatedGenotype = IntStream.range(0, solution.length())
+			.sorted()
+			.mapToDouble(index -> Double.compare(mutationProbabilities[index], mutationRate) <= 0 ?
+				calculateDisturbance(solution.values()[index]) : solution.values()[index])
+			.toArray();
+
+		return solutionFactory.create(mutatedGenotype);
+	}
+
+
+	/**
+	 * Calculates a new value for the mutated gene.
+	 */
+	private double calculateDisturbance(final double value) {
+		// TODO: Trim the new value to the boundaries of the search space.
+		double disturbance = -disturbanceRange * (2 * randomGenerator.nextDouble() - 1);
+		return value + disturbance;
+	}
+
+}
---
+++ ClassicalEvolution/src/main/java/pl/edu/agh/age/de/classical/recombination/RandomDiagonalRecombination.java
@@ -0,0 +1,57 @@
+package pl.edu.agh.age.de.classical.recombination;
+
+import pl.edu.agh.age.compute.stream.emas.reproduction.recombination.Recombination;
+import pl.edu.agh.age.compute.stream.emas.solution.DoubleVectorSolution;
+import pl.edu.agh.age.de.common.solution.DoubleVectorSolutionFactory;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.IntStream;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * This class implements the recombination operator for Classical EMAS Algorithm. This recombination strategy draws a
+ * {@code N}-dimensional point (where {@code N} is the number of genes) located on a segment connecting two
+ * {@code N}-dimensional points representing two genotypes to recombine.
+ *
+ * @author Bartłomiej Grochal
+ */
+public class RandomDiagonalRecombination implements Recombination<DoubleVectorSolution> {
+
+	private final DoubleVectorSolutionFactory solutionFactory;
+	private final Random randomGenerator;
+
+
+	/**
+	 * @param solutionFactory A factory object creating new {@link DoubleVectorSolution solution} objects.
+	 */
+	public RandomDiagonalRecombination(final DoubleVectorSolutionFactory solutionFactory) {
+		this.solutionFactory = checkNotNull(solutionFactory);
+		randomGenerator = ThreadLocalRandom.current();
+	}
+
+
+	/**
+	 * Performs a recombination of two genotypes belonging to given {@code firstSolution} and {@code secondSolution}.
+	 */
+	@Override
+	public DoubleVectorSolution recombine(final DoubleVectorSolution firstSolution, final DoubleVectorSolution secondSolution) {
+		checkArgument(firstSolution.length() == secondSolution.length());
+
+		final double mutationVectorScale = randomGenerator.nextDouble();
+		final double[] mutationVector = IntStream.range(0, firstSolution.length())
+			.sorted()
+			.mapToDouble(index -> mutationVectorScale * (secondSolution.values()[index] - firstSolution.values()[index]))
+			.toArray();
+
+		final double[] offspringGenotype = IntStream.range(0, mutationVector.length)
+			.sorted()
+			.mapToDouble(index -> firstSolution.values()[index] + mutationVector[index])
+			.toArray();
+
+		return solutionFactory.create(offspringGenotype);
+	}
+
+}
