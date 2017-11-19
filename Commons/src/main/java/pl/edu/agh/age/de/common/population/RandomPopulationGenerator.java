5f1dbd1ca0c8582a102ddbbd1a604f75a438ab2f
bgrochal <bartek.grochal@gmail.com>
2017-11-19 21:35:37 +0100

    Population generator and evaluators added.

---
+++ Commons/src/main/java/pl/edu/agh/age/de/common/population/RandomPopulationGenerator.java
@@ -0,0 +1,64 @@
+package pl.edu.agh.age.de.common.population;
+
+import pl.edu.agh.age.compute.stream.emas.EmasAgent;
+import pl.edu.agh.age.compute.stream.emas.PopulationGenerator;
+import pl.edu.agh.age.compute.stream.emas.solution.DoubleVectorSolution;
+import pl.edu.agh.age.de.common.solution.DoubleVectorSolutionFactory;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * This class is responsible for generating an initial population randomly.
+ *
+ * @author Bartłomiej Grochal
+ */
+public class RandomPopulationGenerator implements PopulationGenerator<EmasAgent> {
+
+	private final DoubleVectorSolutionFactory solutionFactory;
+
+	private final int agentsCount;
+	private final int initialEnergy;
+
+
+	/**
+	 * @param solutionFactory A factory object creating new {@link DoubleVectorSolution solution} objects belonging to
+	 *                        the initial population.
+	 * @param agentsCount     A number of individuals to be generated.
+	 * @param initialEnergy   Amount of energy deposited to each of newly-created individuals.
+	 */
+	public RandomPopulationGenerator(final DoubleVectorSolutionFactory solutionFactory, final int agentsCount,
+									 final int initialEnergy) {
+		checkArgument(agentsCount > 0);
+		checkArgument(initialEnergy > 0);
+
+		this.solutionFactory = checkNotNull(solutionFactory);
+
+		this.agentsCount = agentsCount;
+		this.initialEnergy = initialEnergy;
+	}
+
+
+	/**
+	 * Returns a list containing the initial population of {@link EmasAgent agents}.
+	 */
+	@Override
+	public List<EmasAgent> createPopulation() {
+		return IntStream.range(0, agentsCount)
+			.mapToObj(value -> createAgent())
+			.collect(Collectors.toList());
+	}
+
+
+	/**
+	 * Returns single {@link EmasAgent agent} belonging to the initial population.
+	 */
+	private EmasAgent createAgent() {
+		return EmasAgent.create(initialEnergy, solutionFactory.createRandom());
+	}
+
+}
---
+++ Commons/src/main/java/pl/edu/agh/age/de/common/population/evaluator/SimplePopulationEvaluator.java
@@ -0,0 +1,41 @@
+package pl.edu.agh.age.de.common.population.evaluator;
+
+import javaslang.collection.Seq;
+import pl.edu.agh.age.compute.stream.emas.EmasAgent;
+import pl.edu.agh.age.compute.stream.emas.PopulationEvaluator;
+import pl.edu.agh.age.compute.stream.emas.solution.Solution;
+import pl.edu.agh.age.compute.stream.problem.Evaluator;
+
+/**
+ * This class is responsible for evaluating {@link Solution solutions} belonging to {@link EmasAgent agents} composing
+ * a population.
+ *
+ * @author Bartłomiej Grochal
+ */
+public class SimplePopulationEvaluator<S extends Solution<?>> implements PopulationEvaluator<EmasAgent> {
+
+	private final Evaluator<S> evaluator;
+
+
+	/**
+	 * @param evaluator An object implementing the evaluation strategy for particular {@link Solution solutions}.
+	 */
+	public SimplePopulationEvaluator(final Evaluator<S> evaluator) {
+		this.evaluator = evaluator;
+	}
+
+
+	/**
+	 * Returns a list containing all agents belonging to given {@code population} with evaluated fitness values.
+	 */
+	@Override
+	public Seq<EmasAgent> evaluate(final Seq<EmasAgent> population) {
+		return population.map(agent -> {
+			final S solution = (S) agent.solution;
+			solution.updateFitness(evaluator.evaluate(solution));
+
+			return EmasAgent.create(agent.energy, solution);
+		});
+	}
+
+}
