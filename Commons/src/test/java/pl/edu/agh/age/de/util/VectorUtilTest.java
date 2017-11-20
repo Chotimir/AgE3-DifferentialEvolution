d80170e5acc2204d39d4e8f7b9aaad72a632f30b
bgrochal <bartek.grochal@gmail.com>
2017-11-20 23:11:01 +0100

    Tests for vector operations added.

---
+++ Commons/src/test/java/pl/edu/agh/age/de/util/VectorUtilTest.java
@@ -0,0 +1,67 @@
+package pl.edu.agh.age.de.util;
+
+import javaslang.collection.Array;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static pl.edu.agh.age.TestUtils.ACCURACY;
+
+/**
+ * This class contains tests for the {@link VectorUtil} class.
+ *
+ * @author Bartłomiej Grochal
+ */
+public class VectorUtilTest {
+
+	private Array<Double> firstVector;
+	private Array<Double> secondVector;
+	private Array<Double> thirdVector;
+	private Array<Double> shorterVector;
+
+
+	@Before
+	public void setUp() {
+		firstVector = Array.of(-2.0d, -1.0d, 0.0d, 1.0d, 2.0d);
+		secondVector = Array.of(1.0d, 2.0d, 1.0d, 0.0d, -5.0d);
+		thirdVector = Array.of(-1.0d, 1.0d, 1.0d, 1.0d, -3.0d);
+		shorterVector = Array.of(1.0d, 2.0d);
+	}
+
+	@Test
+	public void addVectorsShouldReturnSumOfTwoVectors() {
+		Assert.assertEquals(thirdVector, VectorUtil.addVectors(firstVector, secondVector));
+		Assert.assertEquals(thirdVector, VectorUtil.addVectors(secondVector, firstVector));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void addVectorsShouldThrowIllegalArgumentExceptionForVectorsOfDifferentLengths() {
+		VectorUtil.addVectors(firstVector, shorterVector);
+	}
+
+	@Test
+	public void subtractVectorsShouldReturnDifferenceOfTwoVectors() {
+		Assert.assertEquals(firstVector, VectorUtil.subtractVectors(thirdVector, secondVector));
+		Assert.assertEquals(secondVector, VectorUtil.subtractVectors(thirdVector, firstVector));
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void subtractVectorsShouldThrowIllegalArgumentExceptionForVectorsOfDifferentLengths() {
+		VectorUtil.subtractVectors(firstVector, shorterVector);
+	}
+
+	@Test
+	public void multiplyVectorByScalarShouldReturnScaledVector() {
+		Assert.assertEquals(Array.of(0.0d, 0.0d, 0.0d, 0.0d, 0.0d),
+			VectorUtil.multiplyVectorByScalar(firstVector, 0.0d).map(Math::abs)); // deals with -0.0d values
+		Assert.assertEquals(firstVector, VectorUtil.multiplyVectorByScalar(firstVector, 1.0d));
+		Assert.assertEquals(VectorUtil.addVectors(firstVector, firstVector), VectorUtil.multiplyVectorByScalar(firstVector, 2.0d));
+	}
+
+	@Test
+	public void convertToPrimitiveDoubleArrayShouldReturnArrayOfPrimitives() {
+		Assert.assertArrayEquals(new double[]{-2.0d, -1.0d, 0.0d, 1.0d, 2.0d}, VectorUtil.convertToPrimitiveDoubleArray(firstVector), ACCURACY);
+		Assert.assertArrayEquals(new double[]{}, VectorUtil.convertToPrimitiveDoubleArray(Array.empty()), ACCURACY);
+	}
+
+}
