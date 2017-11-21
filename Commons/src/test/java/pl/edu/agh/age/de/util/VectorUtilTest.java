5b6dd12f2388b26844c5031034710993e197d178
bgrochal <bartek.grochal@gmail.com>
2017-11-21 15:15:43 +0100

    Additional tests cases for vector operations added.

--- Commons/src/test/java/pl/edu/agh/age/de/util/VectorUtilTest.java
+++ Commons/src/test/java/pl/edu/agh/age/de/util/VectorUtilTest.java
@@ -30,6 +30,8 @@ public class VectorUtilTest {
 
 	@Test
 	public void addVectorsShouldReturnSumOfTwoVectors() {
+		Assert.assertEquals(Array.empty(), VectorUtil.addVectors(Array.empty(), Array.empty()));
+
 		Assert.assertEquals(thirdVector, VectorUtil.addVectors(firstVector, secondVector));
 		Assert.assertEquals(thirdVector, VectorUtil.addVectors(secondVector, firstVector));
 	}
@@ -41,6 +43,8 @@ public class VectorUtilTest {
 
 	@Test
 	public void subtractVectorsShouldReturnDifferenceOfTwoVectors() {
+		Assert.assertEquals(Array.empty(), VectorUtil.subtractVectors(Array.empty(), Array.empty()));
+
 		Assert.assertEquals(firstVector, VectorUtil.subtractVectors(thirdVector, secondVector));
 		Assert.assertEquals(secondVector, VectorUtil.subtractVectors(thirdVector, firstVector));
 	}
@@ -52,16 +56,21 @@ public class VectorUtilTest {
 
 	@Test
 	public void multiplyVectorByScalarShouldReturnScaledVector() {
-		Assert.assertEquals(Array.of(0.0d, 0.0d, 0.0d, 0.0d, 0.0d),
-			VectorUtil.multiplyVectorByScalar(firstVector, 0.0d).map(Math::abs)); // deals with -0.0d values
+		Assert.assertEquals(Array.empty(), VectorUtil.multiplyVectorByScalar(Array.empty(), 2.0d));
 		Assert.assertEquals(firstVector, VectorUtil.multiplyVectorByScalar(firstVector, 1.0d));
-		Assert.assertEquals(VectorUtil.addVectors(firstVector, firstVector), VectorUtil.multiplyVectorByScalar(firstVector, 2.0d));
+
+		Assert.assertEquals(Array.of(0.0d, 0.0d, 0.0d, 0.0d, 0.0d),
+			VectorUtil.multiplyVectorByScalar(firstVector, 0.0d).map(Math::abs));	// deals with -0.0d values
+		Assert.assertEquals(Array.of(-4.0d, -2.0d, 0.0d, 2.0d, 4.0d),
+			VectorUtil.multiplyVectorByScalar(firstVector, 2.0d));
+		Assert.assertEquals(Array.of(4.0d, 2.0d, -0.0d, -2.0d, -4.0d),
+			VectorUtil.multiplyVectorByScalar(firstVector, -2.0d));               // note that 0.0d * (-2.0d) = -0.0d
 	}
 
 	@Test
 	public void convertToPrimitiveDoubleArrayShouldReturnArrayOfPrimitives() {
-		Assert.assertArrayEquals(new double[]{-2.0d, -1.0d, 0.0d, 1.0d, 2.0d}, VectorUtil.convertToPrimitiveDoubleArray(firstVector), ACCURACY);
 		Assert.assertArrayEquals(new double[]{}, VectorUtil.convertToPrimitiveDoubleArray(Array.empty()), ACCURACY);
+		Assert.assertArrayEquals(new double[]{-2.0d, -1.0d, 0.0d, 1.0d, 2.0d}, VectorUtil.convertToPrimitiveDoubleArray(firstVector), ACCURACY);
 	}
 
 }
