7b20172cd8c00146307be4b8bd44183c25449187
bgrochal <bartek.grochal@gmail.com>
2018-02-20 18:17:36 +0100

    New predicates added. Missing comments added.

--- Commons/src/main/java/pl/edu/agh/age/de/util/Predicates.java
+++ Commons/src/main/java/pl/edu/agh/age/de/util/Predicates.java
@@ -11,12 +11,35 @@ import java.util.function.Predicate;
  */
 public interface Predicates {
 
+	/**
+	 * Returns the {@link Predicate} which is always {@code true} for any {@link EmasAgent agent}.
+	 */
 	static Predicate<EmasAgent> always() {
 		return agent -> true;
 	}
 
+	/**
+	 * Returns the {@link Predicate} which is {@code true} if and only if given iteration number is not greater than
+	 * {@code iterations}.
+	 */
+	static Predicate<Long> untilIterations(final long iterations) {
+		return iteration -> iteration <= iterations;
+	}
+
+	/**
+	 * Returns the {@link Predicate} which is {@code true} if and only if given iteration number is the multiple of
+	 * {@code iterations}.
+	 */
 	static Predicate<Long> everyIterations(final long iterations) {
 		return iteration -> (iteration % iterations) == 0;
+	}
+
+	/**
+	 * Returns the conjunction of two {@link Predicate predicates}: {@link #everyIterations(long)} with the {@code
+	 * iterationsInterval} argument and {@link #untilIterations(long)} with the {@code maxIterations} argument.
+	 */
+	static Predicate<Long> iterations(final long iterationsInterval, final long maxIterations) {
+		return everyIterations(iterationsInterval).and(untilIterations(maxIterations));
 	}
 
 }
